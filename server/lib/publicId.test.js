d5d27450cdf0fe92657011f423c8661b7f6cf39e
Gavi Lazan <gavriel.lazan@gmail.com>
2026-09-18 11:47:28 +0300

    Revert "test: throwaway failing assertion to verify CI goes red (will revert)"
    This reverts commit 18019f268ef2bbfa8682dc5fddd2dea2b1503c90.

--- server/lib/publicId.test.js
+++ server/lib/publicId.test.js
@@ -29,4 +29,3 @@ describe('generatePublicId', () => {
     expect(ids.size).toBe(1000)
   })
 })
-describe('CI falsifier throwaway', () => { it('deliberately fails', () => { expect(true).toBe(false) }) })
